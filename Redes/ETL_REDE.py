36105550ba75d99e8f08f1b84dc380b9e7966949
Jortieke <joao.jortiekejr@sptech.school>
2026-05-13 09:39:44 -0300

    Integração com Aviation Stack

--- Redes/ETL_REDE.py
+++ Redes/ETL_REDE.py
@@ -1,45 +1,29 @@
 import requests
-import random
 
-def dados_opensky():
-    url = "https://opensky-network.org/api/states/all"
-    response = requests.get(url) #Response é um objeto HTTP
-    response_json = response.json()
-    if response.status_code == 200: #Requisição bem sucedida
-        return response_json
-    else:
-        print("Erro na requisição à API OpenSky Network.")
-        return None
-
-def opensky_timestamp(response_json):
-    return response_json["time"]
-
-def opensky_aeronaves(response_json):
-    total_flights = 0
-    for r in response_json["states"]:
-        if r[2] == "Brazil" or r[2] == "Brasil" or r[2] == "BR":
-            total_flights += 1
-    return total_flights
+def dados_aviationstack():
     
-
-def coletar_banda_processos(total_aeronaves):
-    return {
-        "rastreamento_mbps": round(total_aeronaves * random.uniform(0.4,0.8),2),
-        "rotas_mbps": round(total_aeronaves * random.uniform(0.2,0.5),2),
-        "correlacao_mbps": round(total_aeronaves * random.uniform(0.3,0.7),2),
-        "api_gateway_mbps": round(total_aeronaves * random.uniform(0.1,0.4),2),
-        "bd_mbps": round(total_aeronaves * random.uniform(0.2,0.6),2),
-        "sync_service_mbps": round(total_aeronaves * random.uniform(0.15,0.5),2)
+    params = {
+    'access_key': 'e2326bc56d7d29aab7be45590b9c1aa1',
+    'dep_iata': 'GRU',
+    'limit': 100
     }
 
-def contato_adsb(response_json):
-    tempo_atual = response_json["time"]
-    atualizacao = []
-    for r in response_json["states"]:
-        if r[2] == "Brazil" or r[2] == "Brasil" or r[2] == "BR":
-            ultima_atualizacao = r[4]
-            atualizacao.append(tempo_atual - ultima_atualizacao)
-    return atualizacao 
+    response = requests.get('https://api.aviationstack.com/v1/flights', params = params)
 
-banda = contato_adsb(dados_opensky())
-print(banda)
+    data = response.json()
+    data_aviationstack = data['data']
+    data_api = [["Número do voo", "Status", "Origem", "Destino", "Delay de Partida", "Delay de Chegada"]]
+
+
+    for voo in data_aviationstack:
+        numero_voo = voo.get('flight', {}).get('iata') #IATA é um código composto pela companhia + numero do voo
+        status = voo.get('flight_status') 
+        origem = voo.get('departure', {}).get('airport')
+        destino = voo.get('arrival', {}).get('airport')
+        delay_origem = voo.get('departure', {}).get('delay')
+        delay_destino = voo.get('arrival', {}).get('delay')
+        data_api.append([numero_voo, status, origem, destino, delay_origem, delay_destino])
+
+    return(data_api)
+
+dados_aviationstack()
--- Redes/SCRIPT_CAPTURA_REDE.py
+++ Redes/SCRIPT_CAPTURA_REDE.py
@@ -28,6 +28,34 @@ DB_CONFIG = {
 
 INTERVALO = 1800
 
+#Aviationstack
+
+def dados_aviationstack():
+    
+    params = {
+    'access_key': '',
+    'dep_iata': 'GRU',
+    'limit': 100
+    }
+
+    response = requests.get('https://api.aviationstack.com/v1/flights', params = params)
+
+    data = response.json()
+    data_aviationstack = data['data']
+    data_api = [["Número do voo", "Status", "Origem", "Destino", "Delay de Partida", "Delay de Chegada"]]
+
+
+    for voo in data_aviationstack:
+        numero_voo = voo.get('flight', {}).get('iata') #IATA é um código composto pela companhia + numero do voo
+        status = voo.get('flight_status') 
+        origem = voo.get('departure', {}).get('airport')
+        destino = voo.get('arrival', {}).get('airport')
+        delay_origem = voo.get('departure', {}).get('delay')
+        delay_destino = voo.get('arrival', {}).get('delay')
+        data_api.append([numero_voo, status, origem, destino, delay_origem, delay_destino])
+
+    return(data_api)
+
 def conectar_s3():
     return boto3.client(
         's3',
