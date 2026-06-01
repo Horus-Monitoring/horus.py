87184826c0a704174689399d07851eac11ce139e
jortieke <joao.jortiekejr@sptech.school>
2026-06-01 20:27:07 -0300

    Inserção de DB_PORT no script de captura

--- Captura_Geral_SPRINT 3/Captura.py
+++ Captura_Geral_SPRINT 3/Captura.py
@@ -31,6 +31,7 @@ AWS_CONFIG = {
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
+    "port": int(os.getenv("DB_PORT", 3306)),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_DATABASE")
